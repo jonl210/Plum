557e0dabca3b7195f408e834fb6280bff96726c1
Jonathan <jonathanlambert247@gmail.com>
2018-12-13 09:53:09 -0800

    added minilink model and db connection

--- plum/shortener.py
+++ plum/shortener.py
@@ -1,7 +1,26 @@
 from flask import Blueprint, flash, redirect, render_template, request, url_for
 
+from peewee import *
+
 #Blueprint
 bp = Blueprint('shortener', __name__)
+
+#Database info
+mysql_db = MySQLDatabase('plumdb', user='root', password='dentyne40',
+                    host='localhost', port=3316)
+
+class BaseModel(Model): #Base model
+    class Meta:
+        database = mysql_db
+
+class MiniLink(BaseModel): #Shortened link model
+    original_url = TextField()
+    mini_url = TextField()
+    u_id = CharField(max_length=8)
+
+#Run to add tables to database
+mysql_db.connect()
+mysql_db.create_tables([MiniLink])
 
 @bp.route('/')
 def index():
